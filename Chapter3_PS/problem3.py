3019c3a6845351f33d14ba919b9a51bf3d3dc9a9
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-14 13:17:49 +0000

    Problem 3 is done

--- Chapter3_PS/problem3.py
+++ Chapter3_PS/problem3.py
@@ -1,1 +1,3 @@
-# Question 3 : Write a program to detect doublr space in a string.
+# Question 3 : Write a program to detect double space in a string.
+name = "Piyush is a good  boy and he is very confident about his coding skills"
+print("Index of double space is : ",name.find("  "))
