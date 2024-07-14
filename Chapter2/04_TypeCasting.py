e81293865d655927b29e4b00f6cbc8311f4301aa
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-14 06:56:06 +0000

    Type conversion done

---
+++ Chapter2/04_TypeCasting.py
@@ -0,0 +1,22 @@
+a = 10
+print(type(a))
+
+b = 10.56
+print(type(b))
+
+c = "Strig"
+print(type(c))
+
+d = True
+print(type(d))
+
+e = 'String'
+print(type(e))
+
+f = float(a)
+print(f)
+print(type(f))
+
+g = str(b)
+print(b)
+print(type(g))
