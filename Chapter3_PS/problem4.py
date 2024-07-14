2c4594b7475eeb4a52431c09ff1ddacfe736cd59
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-14 13:21:44 +0000

    Problem 4 is solved

---
+++ Chapter3_PS/problem4.py
@@ -0,0 +1,5 @@
+# Question 4 : Replace double space to single space
+name = "Piyush is a good  boy and he is very confident about his coding skills"
+print(name)
+name1 = name.replace("  "," ");
+print(name1)
