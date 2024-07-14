9dafc209b0d13b682b1e739d9aa5344eb2e736a5
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-14 07:51:04 +0000

    Chapter 3 start

--- Chapter2_PS/problem6.py
+++ Chapter2_PS/problem6.py
@@ -1,3 +1,4 @@
 # Question 6 : Write a program to calcuate the square of a number that entered by the user.
 sqr = int(input("Enter the number : "))
 print("Square of a entered number is : ", sqr * sqr)
+print("Square of a entered number is : ", sqr ** 2)
---
+++ Chapter3/IntroString.py
@@ -0,0 +1,10 @@
+name = "Piyush"
+name1 = 'Mishra'
+name3 = """Hey
+I am
+Piyush
+Mishra"""
+
+print(name)
+print(name1)
+print(name3)
