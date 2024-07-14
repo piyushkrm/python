8f2e9fc3089b760160cc2a7c72cd92ad4f38c554
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-14 07:42:30 +0000

    Problem 6 is solved

---
+++ Chapter2_PS/problem6.py
@@ -0,0 +1,3 @@
+# Question 6 : Write a program to calcuate the square of a number that entered by the user.
+sqr = int(input("Enter the number : "))
+print("Square of a entered number is : ", sqr * sqr)
