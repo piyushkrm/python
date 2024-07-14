fe33ad5fdec78f2e5a90d5086b18f7a1f82ae2df
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-14 10:39:45 +0000

    Problem 1 solved

---
+++ Chapter3_PS/problem1.py
@@ -0,0 +1,3 @@
+# Question 1 : Write a program to display a user entered name followed by Good Afternoon using input function.
+user = input("Please enter your name : ")
+print("Good Afternoon",user)
