2db80bc67752cde254518329e207dc54f76ade99
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-14 07:40:21 +0000

    Problem5 is solved

---
+++ Chapter2_PS/problem5.py
@@ -0,0 +1,5 @@
+# Question 5 : Write a program to find the average of two number that entered by the user :
+nu1 = int(input("Enter number 1 : "))
+nu2 = int(input("Enter number 2 : "))
+
+print("Average of entered number is  : ",(nu1 + nu2)/2)
