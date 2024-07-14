20ed46dcb80f0468bea385d102570d2b1411e63a
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-14 06:48:50 +0000

    Operators done

---
+++ Chapter2/03_operators.py
@@ -0,0 +1,34 @@
+#  Arithmetic Operator
+a = 9
+b = 8
+c = a + b
+print(c)
+
+# Assignment operator
+p = 5 - 9
+print(p)
+
+o = 12
+o += 10
+print(o)
+
+# Comparison Operator
+h = 10 < 59
+print(h)
+k = 10 == 10
+print(k)
+w = 45 < 10
+print(w)
+s = 10 >= 10
+print(s)
+c = 10!=45
+print(c)
+
+# Logical Operator
+y = True or True
+u = True and False
+print(y)
+print(u)
+
+t = not(False)
+print(t)
