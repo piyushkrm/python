ee47888d03cb780e5c5df707abbd811ea360462b
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-16 12:57:19 +0000

    intro done and now moving on methods of list

---
+++ Chapter4/01_list.py
@@ -0,0 +1,12 @@
+item = ["Apple", "orange", 240, 15.2, True]
+print(item)
+print(item[0])
+print(len(item))
+item[3] = 250       # change item
+print(item)         # lists are mutable
+
+for p in item:      # print each element in the item
+    print(p)
+
+print(item[0:3])
+
