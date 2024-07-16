425e88ec4f3e4f8a10db8902297158b1af75dba8
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-16 13:45:21 +0000

    problem 1 solved

---
+++ Chapter4_PS/problem1.py
@@ -0,0 +1,7 @@
+# Question1 : Write a program to store seven fruits in a list entered by the user
+fruits = []
+print("Enter seven fruits name : ")
+for i in range(8):
+    frt = input("Fruits name : ")
+    fruits.append(frt)
+print(fruits)
