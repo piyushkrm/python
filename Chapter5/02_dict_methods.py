333a6b655ff130b8b89b8e11a76da7d5d09038cc
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-16 15:33:35 +0000

    dictionary done

---
+++ Chapter5/02_dict_methods.py
@@ -0,0 +1,15 @@
+marks = {
+    "piyush" : 89,
+    "Aayush" : 90,
+    "sona" : 80,
+    "palak" : 78,
+    "harsh" : 95
+}
+
+# print(marks.items())
+# print(marks.keys())
+# print(marks.pop("sona"))
+# marks.update({"Aayush" : 99, "Prasun" : 92})
+# print(marks)
+print(marks.get("harsh"))
+print(marks["harsh"])
