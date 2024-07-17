c826ac4bf501fc36ff2664c5b40530ad0a7d83e2
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-17 08:11:58 +0000

    problem 8 is solved

---
+++ Chapter5_PS/problem8.py
@@ -0,0 +1,8 @@
+'''If languages of two friends are same; what will happen to the program in problem 6?'''
+dic = {}
+for i in range(4):
+    name = input("Enter name : ")
+    lang = input("Enter language name : ")
+    dic.update({name:lang})
+    
+print(dic)
