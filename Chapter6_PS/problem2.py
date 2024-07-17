0806614c858e5f57a7022cd2c238b0491c799cf9
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-17 10:03:51 +0000

    Another approach of problem 2

--- Chapter6_PS/problem2.py
+++ Chapter6_PS/problem2.py
@@ -1,4 +1,4 @@
-"""Write a program to find out whether a student has passed or failed if it requires a
+"""Question 2: Write a program to find out whether a student has passed or failed if it requires a
 total of 40% and at least 33% in each subject to pass. Assume 3 subjects and
 take marks as an input from the user."""
 
@@ -73,3 +73,16 @@ for sub, scoree in zip(subjectName, obtainedScore):
         print("You are passed in the subject of ",sub)
     elif scoree <= 33:
         print("You are failed in the subject of ",sub)
+
+
+
+# Another one
+a1 = int(input("Enter marks 1"))
+a2 = int(input("Enter marks 2"))
+a3 = int(input("Enter marks 3"))
+
+totalPercentage = (100*(a1 + a2 + a3))/300
+if totalPercentage >= 40 and a1 >= 33 and a2 >= 33 and a3 >= 33:
+    print("You are passed", totalPercentage)
+else:
+    print("You are fail ",totalPercentage)
