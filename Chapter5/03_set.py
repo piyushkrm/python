d780df042c514d6006b5b26a7215d0edcda60b83
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-17 04:30:09 +0000

    Basics set are complete and now move on set methods

---
+++ Chapter5/03_set.py
@@ -0,0 +1,8 @@
+sett = {1, 2, 3, 5, "piyush"}       # set declare with { } braces 
+print(sett)
+empty = set()           # Dont use empty = { } as it will create an empty dictionary
+print(type(empty))
+
+
+notReptSet = {1, 2, 5, 2, 6, 6, 5, "PIHU", "PIHU", "PIHU"}
+print(notReptSet)           #set not print duplicates values availbale in the set and set are not maintain order
