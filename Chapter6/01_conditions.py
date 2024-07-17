f0279f2fbaea1e4212597842ebd03207ce2f8294
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-17 08:31:08 +0000

    Conditionl statement are done

---
+++ Chapter6/01_conditions.py
@@ -0,0 +1,35 @@
+# # if conditon
+# age = int(input("Enter your age : "))
+# if(age >= 18) :
+#     print("You are above the age of consent")
+#     print("Good for you")
+
+
+# # if-else conditon
+# age = int(input("Enter your age : "))
+# if(age >= 18) :
+#     print("You are above the age of consent")
+#     print("Good for you")
+# else:
+#     print("Your are belove the age of content")
+#     print("Not good for you")
+
+
+# # elif conditon
+# age = int(input("Enter your age : "))
+# if(age >= 18) :
+#     print("You are above the age of consent")
+#     print("Good for you")
+# elif(age >= 0):
+#     print("You enterred invalid age")
+# else:
+#     print("Your are belove the age of content")
+#     print("Not good for you")
+
+
+'''Quick Quiz : Write a program to print Yes when the age entered by the user is greater than or equal to 18'''
+age = int(input("Enter the age : "))
+if (age >= 18) :
+    print("Yes")
+else:
+    print("No")
