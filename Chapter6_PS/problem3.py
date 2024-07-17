f379f0ac9ac1b768e991e9d5ac87f87f6fc7fcda
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-17 10:13:22 +0000

    problem 3 is solved

--- Chapter6_PS/problem3.py
+++ Chapter6_PS/problem3.py
@@ -2,3 +2,14 @@
 "Make a lot of money", "buy now", "subscribe this", "click this". Write a program
 to detect these spams.'''
 
+p1 = "Make a lot of money"
+p2 = "buy now"
+p3 = "subscribe this"
+p4 = "click this"
+
+message = input("Enter comment : ")
+msg = message.capitalize()
+if p1 in msg or p2 in msg or p3 in msg or p4 in msg:
+    print("This comment is smap")
+else:
+    print("This is not a spam comment")
