16e1fbdf29f7b22c57cb3e3707cebc55244238f5
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-17 11:58:26 +0000

    Problem 1 in the chapter 7 is solved

---
+++ Chapter7_PS/problem1.py
@@ -0,0 +1,4 @@
+'''Write a program to print multiplication table of a given number using for loop.'''
+table = int(input("Enter number for print these table : "))
+for i in range(1, 11):
+    print(table * i)
