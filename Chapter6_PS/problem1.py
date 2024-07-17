ac8ee25ee70b68802d76d7a708442c98cd05d6f6
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-17 08:39:31 +0000

    Practice set of chapter 6 is started

---
+++ Chapter6_PS/problem1.py
@@ -0,0 +1,6 @@
+"""Question 1 : Write a program to find the greatest of four numbers entered by the user."""
+print("Enter the numbers")
+listt = []
+print(type(listt))
+# for i in range(4):
+#     num = int(input("Enter the number : "))
