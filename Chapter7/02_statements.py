7d48c40cdec259b2ff9021af905ac1428265517b
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-17 11:50:44 +0000

    Break and continue statement

---
+++ Chapter7/02_statements.py
@@ -0,0 +1,10 @@
+for i in range(0, 15):
+    if(i == 10):
+        break           # Exit the loop right now
+    print(i)
+
+    
+for i in range(0, 15):
+    if(i == 10):
+        continue           # skip this iteration
+    print(i)
