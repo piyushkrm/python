89ca2474d3964f3408ffe60e78be55977d5ad370
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-17 05:22:51 +0000

    problem5 is solved

--- Chapter5_PS/problem4.py
+++ Chapter5_PS/problem4.py
@@ -1,12 +1,12 @@
-#  Question 4 : What will be the length of following set s:
-# s = set()
-# s.add(20)
-# s.add(20.0)
-# s.add('20') # length of s after these operations?
+'''Question 4 : What will be the length of following set s:
+s = set()
+s.add(20)
+s.add(20.0)
+s.add('20') # length of s after these operations? '''
 
 se = set()
 se.add(20)
 se.add(20.0)
 se.add('20') 
-print(len(se))
+print(len(se))          #output is 2 because in python 20 == 20.0 is equal. python compare value and values are same then don't yhink about data type so output is 2 is True
 print(se)
