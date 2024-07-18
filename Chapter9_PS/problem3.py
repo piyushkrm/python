736e138e7568ebd5172290580b5b9c533a779689
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-18 13:36:26 +0000

    problem 3 is solved

---
+++ Chapter9_PS/problem3.py
@@ -0,0 +1,13 @@
+'''Write a program to generate multiplication tables from 2 to 20 and write it to the
+different files. Place these files in a folder for a 13- year old.'''
+
+def generateTable(n) :
+    table = ""
+    for i in range(1, 11) :
+        table += f"{n} x {i} : {n*i}\n"
+
+    with open(f"/workspaces/python/Chapter9_PS/tables/table_{n}.txt", "w") as f:
+        f.write(table)
+
+for i in range(2, 21) :
+    generateTable(i)
