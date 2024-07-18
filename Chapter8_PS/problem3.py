0142cf43b5e7e6b0b4022379ec718b5402f7b40a
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-18 05:31:36 +0000

    Problem 3 is solved

---
+++ Chapter8_PS/problem3.py
@@ -0,0 +1,7 @@
+'''How do you prevent a python print() function to print a new line at the end.'''
+print("P",end="")
+print("I",end="")
+print("Y",end="")
+print("U",end="")
+print("S",end="")
+print("H",end="")
