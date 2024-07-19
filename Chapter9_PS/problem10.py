d4124737a910672f624b425188c5c46fd3a141e6
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-19 05:41:20 +0000

    Problem 10 is solved

---
+++ Chapter9_PS/problem10.py
@@ -0,0 +1,7 @@
+'''Write a program to wipe out the content of a file using python.'''
+
+
+with open("/workspaces/python/Chapter9_PS/10_wipeOut.txt", "w") as f:
+    f.write("")
+
+#  remove text from "10_wipeOut.txt" file
