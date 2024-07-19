9a7837b67e99e4da3b4b9fc6c2d22a48b48dccd9
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-19 04:42:42 +0000

    Problem 4 is resolved again

--- Chapter9_PS/problem4.py
+++ Chapter9_PS/problem4.py
@@ -1,9 +1,10 @@
 '''A file contains a word "Donkey" multiple times. You need to write a program which
 replace this word with ##### by updating the same file.'''
 
+word = "Donkey"
+with open("/workspaces/python/Chapter9_PS/4_dunkey.txt", "r") as f:
+    content = f.read()  # Read the entire file
 
-with open("/workspaces/python/Chapter9_PS/4_dunkey.txt", "r+") as f:
-    content = f.readlines()  # Read the entire file
-    f.seek(0)  # Move the file pointer back to the beginning
-    f.writelines(line.replace("Donkey", "#####") for line in content)  # Replace and write the content
-    f.truncate()  # Remove any remaining content
+newContent = content.replace(word, "#####")
+with open("/workspaces/python/Chapter9_PS/4_dunkey.txt", "w") as f:
+    f.write(newContent)
